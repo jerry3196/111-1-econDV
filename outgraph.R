aa119f5e886e3b83a2a48fc619179ffcaf692dbf
jerry3196 <92929181+jerry3196@users.noreply.github.com>
2022-11-23 17:41:18 +0800

    Update outgraph.R

--- outgraph.R
+++ outgraph.R
@@ -1,51 +1,31 @@
 library(dplyr)
 library(ggplot2)
 X1 <- read.csv("C:/Users/User/Desktop/HPM/hp/X1_2.csv")
-X1 %>% filter(X1$mindistance1 <= 0.2)  %>% mutate(距離="200m")-> X1_200
-X1 %>% filter(X1$mindistance1 <= 0.4 & X1$mindistance1 > 0.2) %>% mutate(距離="400m") -> X1_400
-X1 %>% filter(X1$mindistance1 <= 0.6 & X1$mindistance1 > 0.4) %>% mutate(距離="600m")-> X1_600
-X1 %>% filter(X1$mindistance1 <= 0.8 & X1$mindistance1 > 0.6) %>% mutate(距離="800m")-> X1_800
-X1 %>% filter(X1$mindistance1 <= 1 & X1$mindistance1 > 0.8)  %>% mutate(距離="1000m") -> X1_1000
-#X1 %>% filter(X1$mindistance1 > 1) %>% mutate(mindistance1x=6)-> X1_1001
-X1 <- rbind(X1_200,X1_400)
-X1 <- rbind(X1,X1_600)
-X1 <- rbind(X1,X1_800)
-X1 <- rbind(X1,X1_1000)
-#X1 <- rbind(X1,X1_1001)
-X1 %>% mutate(數量=X1$mindistance2) -> X1
-X1 %>% mutate(設施="教堂") -> X1_1
-X1 <- read.csv("C:/Users/User/Desktop/HPM/hp/X1_2.csv")
-X1 %>% filter(X1$mindistance2 <= 0.2)  %>% mutate(距離="200m")-> X1_200
-X1 %>% filter(X1$mindistance2 <= 0.4 & X1$mindistance2 > 0.2) %>% mutate(距離="400m") -> X1_400
-X1 %>% filter(X1$mindistance2 <= 0.6 & X1$mindistance2 > 0.4) %>% mutate(距離="600m")-> X1_600
-X1 %>% filter(X1$mindistance2 <= 0.8 & X1$mindistance2 > 0.6) %>% mutate(距離="800m")-> X1_800
-X1 %>% filter(X1$mindistance2 <= 1 & X1$mindistance2 > 0.8)  %>% mutate(距離="1000m") -> X1_1000
-#X1 %>% filter(X1$mindistance2 > 1) %>% mutate(mindistance1x=6)-> X1_1001
-X1 <- rbind(X1_200,X1_400)
-X1 <- rbind(X1,X1_600)
-X1 <- rbind(X1,X1_800)
-X1 <- rbind(X1,X1_1000)
-#X1 <- rbind(X1,X1_1001)
-X1 %>% mutate(數量=X1$mindistance1) -> X1
-X1 %>% mutate(設施="寺廟") -> X1_2
-X1 <- read.csv("C:/Users/User/Desktop/HPM/hp/X1_2.csv")
-X1 %>% filter(X1$mindistance3 <= 0.2)  %>% mutate(距離="200m")-> X1_200
-X1 %>% filter(X1$mindistance3 <= 0.4 & X1$mindistance3 > 0.2) %>% mutate(距離="400m") -> X1_400
-X1 %>% filter(X1$mindistance3 <= 0.6 & X1$mindistance3 > 0.4) %>% mutate(距離="600m")-> X1_600
-X1 %>% filter(X1$mindistance3 <= 0.8 & X1$mindistance3 > 0.6) %>% mutate(距離="800m")-> X1_800
-X1 %>% filter(X1$mindistance3 <= 1 & X1$mindistance3 > 0.8) %>% mutate(距離="1000m") -> X1_1000
-#X1 %>% filter(X1$mindistance3 > 1) %>% mutate(mindistance1x=6)-> X1_1001
-X1 <- rbind(X1_200,X1_400)
-X1 <- rbind(X1,X1_600)
-X1 <- rbind(X1,X1_800)
-X1 <- rbind(X1,X1_1000)
-#X1 <- rbind(X1,X1_1001)
-X1 %>% mutate(數量=X1$mindistance3) -> X1
-X1 %>% mutate(設施="清真寺") -> X1_3
-X1<-rbind(X1_1,X1_2)
-X1<-rbind(X1,X1_3)
-
-ggplot(data=X1,mapping=aes(x=距離, y=設施))+
-  geom_point(shape=20,alpha=0.5,mapping=aes(color=距離,size=數量))+
-  geom_point(#shape=20,#size=1,
-  mapping=aes(color=距離))
+X1 %>% filter(X1$mindistance1 <= 0.2)  %>% mutate(距離=200)-> X1_2001
+X1 %>% filter(X1$mindistance1 <= 0.4 & X1$mindistance1 > 0.2) %>% mutate(距離=400) -> X1_4001
+X1 %>% filter(X1$mindistance1 <= 0.6 & X1$mindistance1 > 0.4) %>% mutate(距離=600)-> X1_6001
+X1 %>% filter(X1$mindistance1 <= 0.8 & X1$mindistance1 > 0.6) %>% mutate(距離=800)-> X1_8001
+X1 %>% filter(X1$mindistance1 <= 1 & X1$mindistance1 > 0.8)  %>% mutate(距離=1000) -> X1_10001
+X1 %>% filter(X1$mindistance2 <= 0.2)  %>% mutate(距離=200)-> X1_2002
+X1 %>% filter(X1$mindistance2 <= 0.4 & X1$mindistance2 > 0.2) %>% mutate(距離=400) -> X1_4002
+X1 %>% filter(X1$mindistance2 <= 0.6 & X1$mindistance2 > 0.4) %>% mutate(距離=600)-> X1_6002
+X1 %>% filter(X1$mindistance2 <= 0.8 & X1$mindistance2 > 0.6) %>% mutate(距離=800)-> X1_8002
+X1 %>% filter(X1$mindistance2 <= 1 & X1$mindistance2 > 0.8)  %>% mutate(距離=1000) -> X1_10002
+X1 %>% filter(X1$mindistance3 <= 0.2)  %>% mutate(距離=200)-> X1_2003
+X1 %>% filter(X1$mindistance3 <= 0.4 & X1$mindistance3 > 0.2) %>% mutate(距離=400) -> X1_4003
+X1 %>% filter(X1$mindistance3 <= 0.6 & X1$mindistance3 > 0.4) %>% mutate(距離=600)-> X1_6003
+X1 %>% filter(X1$mindistance3 <= 0.8 & X1$mindistance3 > 0.6) %>% mutate(距離=800)-> X1_8003
+X1 %>% filter(X1$mindistance3 <= 1 & X1$mindistance3 > 0.8) %>% mutate(距離=1000) -> X1_10003
+X <- data.frame(距離.公尺=c(200,200,200,400,400,400,600,600,600,800,800,800,1000,1000,1000),
+                數量=c(length(X1_2001$state),length(X1_2002$state),length(X1_2003$state),
+                length(X1_4001$state),length(X1_4002$state),length(X1_4003$state),
+                length(X1_6001$state),length(X1_6002$state),length(X1_6003$state),
+                length(X1_8001$state),length(X1_8002$state),length(X1_8003$state),
+                length(X1_10001$state),length(X1_10002$state),length(X1_10003$state)),
+                設施=c("教堂","寺廟","清真寺"))
+ggplot(data = X, aes(x = 距離.公尺, y = 數量, fill = 設施)) +
+  geom_bar(stat = "identity")+
+  scale_x_continuous(breaks=seq(0,1000,200),limits=c(100,1100))+
+  labs(title = "房地產距離最近各宗教設施之距離分布圖",
+       subtitle = "2013-2019",
+       caption = "Source:本研究自行整理")
